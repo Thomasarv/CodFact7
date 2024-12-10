32df1187aedb3ea48757b31337305be1b35bb691
Th.Arvaniths <thomas99arv@gmail.com>
2024-12-10 20:01:15 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,30 @@
+package gr.aueb.cf.ch1;
+
+/**
+ *  Prints Hello Coding Factory in
+ *  standard output (console)
+ *
+ * @author t123
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+//Prints Hello Coding Factory
+        /*
+
+         * Prints in standard output
+         * "Hello Coding Factory"
+         */
+
+
+
+
+        System.out.println("Hello Coding Factory!"); //Prints Hello Coding Factory
+        System.out.print("Hello World!");
+        System.out.println();
+        System.out.printf("");
+
+
+    }
+}
